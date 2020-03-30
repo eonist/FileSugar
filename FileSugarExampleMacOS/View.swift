e9c3917999f9874a2e1cf4f5a895639421b25407
eonist <30n1st@gmail.com>
2020-03-30 15:28:28 +0200

    Clean up project for SPM

--- FileSugarExampleMacOS/View.swift
+++ FileSugarExampleMacOS/View.swift
@@ -1,32 +1,33 @@
 import Cocoa
-import FileSugar_macOS
 
 open class View: NSView {
-    override open var isFlipped: Bool { return true } // TopLeft orientation
-    override public init(frame: CGRect) {
-        super.init(frame: frame)
-        Swift.print("hello world")
-        test1()
-        self.wantsLayer = true // if true then view is layer backed
-    }
-    /**
-     * Boilerplate
-     */
-    @available(*, unavailable)
-    public required init?(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+   override open var isFlipped: Bool { true } // TopLeft orientation
+   override public init(frame: CGRect) {
+      super.init(frame: frame)
+      Swift.print("hello world")
+      test1()
+      self.wantsLayer = true // if true then view is layer backed
+   }
+   /**
+    * Boilerplate
+    */
+   @available(*, unavailable)
+   public required init?(coder decoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+   }
 }
-
+/**
+ * Test
+ */
 extension View {
-    /**
-     * Testing getting content from folder
-     */
-    func test1() {
-        let result = FileParser.content(dirPath: NSString(string: "~/Desktop/").expandingTildeInPath)
-        Swift.print("result.count:  \(result?.count)")
-        result?.forEach {
-            Swift.print("$0:  \($0)") // prints all the files on the desktop folder
-        }
-    }
+   /**
+    * Testing getting content from folder
+    */
+   func test1() {
+      let result = FileParser.content(dirPath: NSString(string: "~/Desktop/").expandingTildeInPath)
+      Swift.print("result.count:  \(String(describing: result?.count))")
+      result?.forEach {
+         Swift.print("$0:  \($0)") // prints all the files on the desktop folder
+      }
+   }
 }
---
+++ Sources/FileSugar/stream/FileStreamReader.swift
@@ -0,0 +1,68 @@
+import Foundation
+/**
+ * Reads data from a file (Continiously)
+ */
+public class FileStreamReader {
+   /**
+    * Reads data from filepath
+    * ## Examples:
+    * let filePath: String = NSString(string: "~/Desktop/del.txt").expandingTildeInPath
+    * let data:Data = FileStreamReader.read(filePath: filePath, startIndex: 50, endIndex: 100)
+    * Swift.print("\(String(data: data, encoding: .utf8))") // blalbslalballabalbla...
+    * Fixme: ⚠️️ Use UInt64 on endIndex
+    * Fixme: ⚠️️ Use Result type
+    */
+   public static func read(url: URL, startIndex: UInt64, endIndex: Int) throws -> Data {
+      do {
+         let file: FileHandle = try .init(forReadingFrom: url)
+         file.seek(toFileOffset: startIndex)
+         let length: Int = endIndex - Int(startIndex)
+//         Swift.print("length:  \(length)")
+         let databuffer = file.readData(ofLength: length)
+         file.closeFile()
+         return databuffer
+      } catch {
+         throw NSError(domain: ("Error: \(error) reading \(url.path)"), code: 0)
+      }
+   }
+}
+//let data = fileHandle?.readDataToEndOfFile()
+extension FileStreamReader {
+   /**
+    * Returns filesize for a filePath
+    * ## Examples:
+    * let fileSize = FileStreamReader.fileSize(filePath: filePath)
+    * - Note: same as doing `data.count`
+    */
+   public static func fileSize(filePath: String) throws -> UInt64 {
+      let fileUrl = URL(fileURLWithPath: filePath)
+      let fileManager = FileManager.default
+      do {
+         let attributes = try fileManager.attributesOfItem(atPath: (fileUrl.path))
+         var fileSize = attributes[FileAttributeKey.size] as! UInt64
+         let dict = attributes as NSDictionary
+         fileSize = dict.fileSize()
+         return fileSize
+      }
+      catch let error as NSError {
+         throw NSError(domain: ("Something went wrong: \(error)"), code: 0)
+      }
+   }
+}
+extension FileStreamReader {
+   /**
+    * Support for filePath
+    */
+   public static func read(filePath: String, startIndex: UInt64, endIndex: Int) throws -> Data {
+      let url: URL = .init(fileURLWithPath: filePath)
+      return try read(url: url, startIndex: startIndex, endIndex: endIndex)
+   }
+   /**
+    * Read string
+    */
+   static func read(filePath: String, start: UInt64, end: Int) throws -> String {
+      let data: Data = try FileStreamReader.read(filePath: filePath, startIndex: start, endIndex: end)
+      guard let string = String(data: data, encoding: .utf8) else { throw NSError(domain: "FileStreamReader.read() - Unable to get string from data data.count: \(data.count)", code: 0) }
+      return string
+   }
+}
