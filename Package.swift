79df97e96fed40a4f45fd3ce43c931d3be124889
eonist <30n1st@gmail.com>
2023-03-23 05:37:03 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "FileSugar",
+            platforms: [.iOS(.v12), .macOS(.v10_13)],
             targets: ["FileSugar"])
     ],
     dependencies: [
